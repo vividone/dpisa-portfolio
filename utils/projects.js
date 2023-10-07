189ded33588ae2064c847fecce567b45ab5700a8
Victor Olaitan <vividvictor@gmail.com>
2023-10-07 17:53:55 +0100

    New Images

--- utils/projects.js
+++ utils/projects.js
@@ -13,7 +13,7 @@ export const projects = [
 		liveUrl: "https://dapoabiodun.com/",
 		description:
 			"This is the Official Website of Prince Dapo Abiodun MFR. The 5th Executive Governor of Ogun State and the Dapo Abiodun Political Organisation. ",
-		images: [{ original: "/projects/dapoabiodun.jpeg"}]
+		images: [{ original: "/projects/dapoabiodun.jpeg" }]
 	},
 	{
 		id: "nogicjqs",
@@ -21,7 +21,7 @@ export const projects = [
 		liveUrl: "https://nogicjqs.gov.ng/",
 		description:
 			"Nigerian Oil and Gas Industry Joint Qualification system (NOGICJQS) This project is built on Laravel, Processmaker to manage the operational workflows and mysql has database. ",
-		images: [{ original: "/projects/nogicjqs.jpeg"}]
+		images: [{ original: "/projects/nogicjqs.jpeg" }]
 	},
 	{
 		id: "sketch",
@@ -29,7 +29,7 @@ export const projects = [
 		liveUrl: "https://iconnectng.com/",
 		description:
 			"An e-commerce platform for an Authorized Apple Reseller built with WordPress and WooCommerce",
-		images: [{ original: "/projects/iconnect.jpeg"}]
+		images: [{ original: "/projects/iconnect.jpeg" }]
 	},
 	{
 		id: "invision",
@@ -37,7 +37,7 @@ export const projects = [
 		liveUrl: "https://NPSAwards.ng/",
 		description:
 			"NPSAwards.ng is a website that provides information about the Nigerian Prize for Science (NPS), the highest scientific award in Nigeria. The website includes information about the nomination process, the eligibility criteria, and the past winners of the award.",
-		images: [{ original: "/projects/NPSAwards.jpeg"}]
+		images: [{ original: "/projects/NPSAwards.jpeg" }]
 	},
 	{
 		id: "figma",
@@ -45,15 +45,23 @@ export const projects = [
 		liveUrl: "https://TGL.ng",
 		description:
 			"TGL.ng is a website that provides information and resources on technology, gadgets, and lifestyle in Nigeria. The website includes articles on a variety of topics, such as smartphones, laptops, and the latest trends in technology.",
-		images: [{ original: "/projects/TGL.jpeg"}]
+		images: [{ original: "/projects/TGL.jpeg" }]
 	},
+	// {
+	// 	id: "pricepredictor",
+	// 	title: "PredictCoin",
+	// 	liveUrl: "https://predictcoin.finance",
+	// 	description:
+	// 		"PredictCoin is an all-in-one price prediction DAO. Stake, Farm, Vote and Predict the prices of various crypto assets. Save, earn passive income and learn a thing or two from professional predictors.			",
+	// 	images: [{ original: "/projects/pricepred.jpeg" }]
+	// },
 	{
 		id: "awesome",
 		title: "FSDH Group ",
 		liveUrl: "https://fsdhgroup.com/",
 		description:
 			"FSDH Group is a financial services group that provides a range of services, including investment banking, asset management, and research. The company's website provides information about its products and services, as well as news and analysis on the financial markets.        ",
-		images: [{ original: "/projects/fsdh.jpeg"}]
+		images: [{ original: "/projects/fsdh.jpeg" }]
 	},
 	{
 		id: "light",
@@ -61,7 +69,7 @@ export const projects = [
 		liveUrl: "https://theaviatorsafrica.com/",
 		description:
 			"Aviators Africa is a pan African resourceful and integrated media, marketing and consulting company for aviation, travel and tourism sectors. The website was custom built with WordPress and Elementor.        ",
-		images: [{ original: "/projects/Aviators.jpeg"}]
+		images: [{ original: "/projects/Aviators.jpeg" }]
 	},
 	{
 		id: "connect",
@@ -69,7 +77,7 @@ export const projects = [
 		liveUrl: "https://lydiawilsonfoundation.org/",
 		description:
 			"Lydia Wilson Foundation is a non-profit, non-religious, non-political and an international non-governmental organisation working to improve the living conditions of the vulnerable which include Internally displaced Persons (IDPs), orphans, widow(er)s and aged people living in our communities through direct programmes such as education, agriculture, food security, health and micro-finance.",
-		images: [{ original: "/projects/Lydia.jpeg"}]
+		images: [{ original: "/projects/Lydia.jpeg" }]
 	},
 	{
 		id: "walk",
@@ -77,21 +85,22 @@ export const projects = [
 		liveUrl: "https://walkforloveafrica.org/",
 		description:
 			"Walk For Love Africa (WFL) Is A Non-Profit Organisation And A Pioneer In Promoting Intra-Africa Connectivity Towards Creating A Central Domestic African Market For Trade And Tourism.",
-		images: [{ original: "/projects/WFL.jpeg"}]
+		images: [{ original: "/projects/WFL.jpeg" }]
 	},
-	// 	{
-	// 		id: "pension",
-	// 		title: "Pensions Alliance Limited (PAL Pensions)",
-	// 		liveUrl: "https://www.palpensions.com/",
-	// 		description:
-	// 			"Pensions Alliance Limited (PAL Pensions) is a joint venture between FSDH Group and African Alliance Insurance Company Limited. PAL Pensions is a pension fund administrator that provides pension management services to individuals and organizations.",
-	// image: "/projects/PAL.jpeg"	},
+	{
+		id: "pension",
+		title: "Pensions Alliance Limited (PAL Pensions)",
+		liveUrl: "https://www.palpensions.com/",
+		description:
+			"Pensions Alliance Limited (PAL Pensions) is a joint venture between FSDH Group and African Alliance Insurance Company Limited. PAL Pensions is a pension fund administrator that provides pension management services to individuals and organizations.",
+		image: "/projects/PAL.jpeg"
+	},
 	{
 		id: "unotelos",
 		title: "Unotelos",
 		liveUrl: "https://unotelos.com/",
 		description:
 			"Unotelos is at the forefront of every technological revolution, system integrations, mobile network solutions, surveillance, and security, including 5G and AI across 9 African markets. The website was built with NextJS and Strapi Headless CMS. ",
-		images: [{ original: "/projects/Unotelos.jpeg"}]
+		images: [{ original: "/projects/Unotelos.jpeg" }]
 	}
 ];
